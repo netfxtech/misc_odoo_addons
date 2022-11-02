8ca5074a68c3cf3224a2c0a9e1129c565a2f8372
hai_ <62037189+netfxtech@users.noreply.github.com>
2022-11-02 10:20:55 -0700

    include security groups in manifest

--- access_res_partner_contractor/__manifest__.py
+++ access_res_partner_contractor/__manifest__.py
@@ -11,6 +11,7 @@
     'version': '0.1',
     'depends': ['base'],
     'data': [
+        'security/security_groups.xml',
         'views/res_partner_views.xml',
         'views/res_users_views.xml',
     ],
